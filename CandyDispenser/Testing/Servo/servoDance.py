bf9fdb9cc84bf4c75207166c6eaccc985c514ccc
BruceClark406 <brucewestonmt@gmail.com>
2018-12-11 10:38:29 -0700

    Changed Time on ServoTest

--- CandyDispenser/Testing/Servo/servoDance.py
+++ CandyDispenser/Testing/Servo/servoDance.py
@@ -11,9 +11,9 @@ p.start(7.5)
 try:
     while True:
         p.ChangeDutyCycle(7.5)
-        time.sleep(1)
+        time.sleep(3)
         p.ChangeDutyCycle(12.5)
-        time.sleep(1)
+        time.sleep(2)
         p.ChangeDutyCycle(2.5)
         time.sleep(1)
 
